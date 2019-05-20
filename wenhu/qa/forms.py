65de4b99a82541556d78608f728fc5d5f43a8fbe
yktimes <1162073988@qq.com>
2019-05-20 16:38:44 +0800

    问答 提问回答功能实现

--- wenhu/qa/forms.py
+++ wenhu/qa/forms.py
@@ -2,14 +2,17 @@ from django import forms
 
 from markdownx.fields import MarkdownxFormField
 
-from wenhu.articles.models import Article
+from wenhu.qa.models import Vote,Question
 
-#
-# class ArticleForm(forms.ModelForm):
-#     status = forms.CharField(widget=forms.HiddenInput())  # 隐藏
-#     edited = forms.BooleanField(widget=forms.HiddenInput(), required=False, initial=False)  # 隐藏
-#     content = MarkdownxFormField()
-#
-#     class Meta:
-#         model = Article
-#         fields = ["title", "content", "image", "tags", "status", "edited"]
+
+class QuestionForm(forms.ModelForm):
+    status = forms.CharField(widget=forms.HiddenInput())  # 隐藏
+
+    content = MarkdownxFormField()
+
+    class Meta:
+        model = Question
+        fields = ["title", "content",  "tags", "status", ]
+
+
+
--- wenhu/qa/views.py
+++ wenhu/qa/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.views.generic import ListView, CreateView, UpdateView, DetailView, DetailView
-from wenhu.qa.models import Question,Vote
+from wenhu.qa.models import Question,Vote,Answer
 from django.urls import reverse_lazy
 from django.core.exceptions import PermissionDenied
 from django.urls import reverse
 from wenhu.helpers import AuthorRequireMixin
 from wenhu.helpers import ajax_required
+from .forms import QuestionForm
 
 
 
@@ -48,3 +49,43 @@ class UnAnsweredQuestionListView(QuestionListView):
         context["active"] = "unanswered" # 这是给前端 Tab 栏设置的
 
         return context
+
+
+
+class CreateQuestionView(LoginRequiredMixin,CreateView):
+
+    form_class = QuestionForm
+    template_name = 'qa/question_form.html'
+    message = "问题已提交"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(CreateQuestionView,self).form_valid(form)
+
+    def get_success_url(self):
+        messages.success(self.request,self.message)
+        return reverse_lazy("qa:all_q")
+
+
+class QuestionDetailView(LoginRequiredMixin,DetailView):
+    model = Question
+    context_object_name = 'question'
+    template_name = 'qa/question_detail.html'
+
+
+class CreateAnswerView(LoginRequiredMixin,CreateView):
+
+    model = Answer
+    fields = ('content',)
+    message = "您的问题已提交"
+    template_name = 'qa/answer_form.html'
+
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form .instance.question_id = self.kwargs['question_id']
+        return super(CreateAnswerView,self).form_valid(form)
+
+    def get_success_url(self):
+        messages.success(self.request,self.message)
+        return reverse_lazy('qa:question_detail',kwargs={"pk":self.kwargs['question_id']})
